9daaaaf95142e1e4a72346cabfbbdd7aa05633bc
taiga-automate-growth <oshima@automate-growth.com>
2024-09-08 20:27:03 +0900

    add:インスタンスメソッド用のシグネチャと同じグローバルダミー関数を追加 インスタンスメソッドのコード補完のため

---
+++ src/InstanceMethod.js
@@ -0,0 +1,17 @@
+/**
+ * コンテンツを生成する
+ * @param {string} prompt - モデルへの指示
+ * @param {Blob} media - モデルに追加で渡す画像、音声、動画のBlob
+ */
+function generateContent(prompt, media = null){
+    throw new Error('GeminiApiClientのダミーメソッドです');
+}
+
+/**
+ * モデルを変更する
+ * @param {'1.5flash' | '1.5pro' | '1.0pro'} model - 利用するAIモデル
+ * @return {GeminiApiClient}
+ */
+function changeModel(model){
+    throw new Error('GeminiApiClientのダミーメソッドです');
+}
