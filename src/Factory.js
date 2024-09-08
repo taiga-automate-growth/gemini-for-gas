9167a0326ef1e2495c60830d91c22cd9853ceaea
taiga-automate-growth <oshima@automate-growth.com>
2024-09-08 20:26:15 +0900

    add: ファクトリーメソッドを追加 ライブラリ内部のクラスは直接参照できないため

---
+++ src/Factory.js
@@ -0,0 +1,10 @@
+/**
+ * Geminiを生成する
+ * @param {string} apikey - APIキー
+ * @param {'1.5flash' | '1.5pro' | '1.0pro'} model - 利用するAIモデル
+ * @return {GeminiApiClient}
+ */
+function create(apikey,model){
+    const gemini = new Gemini(apikey, model);
+    return gemini;
+}
